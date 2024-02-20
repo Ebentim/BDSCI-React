0aeeadf3601a54da35f6b8d7547d98ce18608505
Timilehin Olayuwa <timileyinolayuwa@gmail.com>
2024-02-20 22:11:33 +0100

    jwt access tokens

--- src/components/CourseBody.js
+++ src/components/CourseBody.js
@@ -13,55 +13,188 @@ import ChapterTwelve from "../chapters/ChapterTwelve";
 import ChapterThirteen from "../chapters/ChapterThirteen";
 import ChapterFourteen from "../chapters/ChapterFourteen";
 import ChapterFifteen from "../chapters/ChapterFifteen";
-import "../styles/general.css"
+import PrivateRoute from "./PrivateRoute";
+import "../styles/general.css";
 import Timer from "../components/Timer";
 import { NavButtons } from "../Assets/next";
 import { useReducer, useState } from "react";
-const reducer = (value, action) =>{
-    switch (action.type){
-      case "plus":
-        return ++value;
-        break;
-      case "minus":
-        return  --value;
-    }
-}
-export default function CourseBod() {
-  const startChapter = 1
-  const [chapter, setChapter] = useReducer(reducer, startChapter);
-  const [disableNextButton, setDisableNextButton] = useState(true) // This is meant to disable the next chapter, if the student doesn't scale the current chapter's quiz by at least 80%
-
-
-
-const handleNext = ()=>{
-  if (chapter < 15 || (chapter === 15 && localStorage.getItem("lastTime") === 0 && chapter < 17)){
-    setChapter({type: "plus"})
+import { redirect, Route } from "react-router-dom";
+const reducer = (value, action) => {
+  switch (action.type) {
+    case "plus":
+      return ++value;
+      break;
+    case "minus":
+      return --value;
   }
-  console.log(chapter)
-}
-
-const handPrev = ()=>{
-  if (chapter > 1){
-    setChapter({type: "minus"})} // Activated Chapter only
-  console.log(chapter)
-}
-
-
-  const NextChapter =()=>{
-    return <NavButtons onclick={handleNext} classname="nextButton" disable={disableNextButton}>To  {chapter ===1? "Unit Two" : chapter === 2? "Unit Three" : chapter ===3? "Unit Four" : chapter === 4? "Unit Five" : chapter === 5 ? "Unit Six" : chapter === 6? "Unit Seven" : chapter === 7? "Unit Eight" : chapter === 8? "Unit Nine" : chapter === 9 ? "Unit Ten" : chapter === 10? "Unit Eleven" : chapter === 11 ? "Unit Twelve" : chapter === 12 ? "Unit Thirteen" : chapter ===13? "Unit Fourteen" : chapter ===14 ? "Unit Fifteen" : "Final Quiz" }</NavButtons>
-  }
-
-  const PreviousChapter = ()=>{
-  return <NavButtons onclick={handPrev} classname="nextButton">Go back to {chapter=== 16? "Unit Fifteen" : chapter ===15? "Unit Fourteen" : chapter === 14? "Unit Thirteen" : chapter ===13? "Unit Twelve" : chapter === 12? "Unit Eleven" : chapter === 11 ? "Unit Ten" : chapter === 10? "Unit Nine" : chapter === 9? "Unit Eight" : chapter === 8? "Unit Seven" : chapter === 7 ? "Unit Six" : chapter === 6? "Unit Five" : chapter === 5 ? "Unit Four" : chapter === 4 ? "Unit Three" : chapter ===3? "Unit Two" : chapter === 2 ? "Unit 1" : "Hi"}</NavButtons>
-}
-  
-  return (
-      <div>
-        <div className="TimerContainer">
-          <Timer />
-        </div>
-        {chapter === 1? (<div className="NextButtonContainer"><NextChapter/></div>) : (<div className="nextButtonContainer"><PreviousChapter /> <NextChapter/></div>)}
-        {chapter === 1? <ChapterOne/> : chapter === 2? <ChapterTwo/> : chapter === 3? <ChapterThree/> : chapter === 4? <ChapterFour/> : chapter === 5? <ChapterFive/> : chapter === 6? <ChapterSix/> : chapter === 7? <ChapterSeven/> : chapter === 8? <ChapterEight/> : chapter === 9? <ChapterNine/> : chapter === 10? <ChapterTen/> : chapter === 11? <ChapterEleven/> : chapter === 12? <ChapterTwelve/> : chapter === 13? <ChapterThirteen/> : chapter === 14? <ChapterFourteen/> : chapter === 15? <ChapterFifteen/> : null}
-      </div>
-  );
 };
+export default function CourseBod() {
+  const startChapter = 1;
+  const [chapter, setChapter] = useReducer(reducer, startChapter);
+  const [disableNextButton, setDisableNextButton] = useState(true); // This is meant to disable the next chapter, if the student doesn't scale the current chapter's quiz by at least 80%
+
+  const handleNext = () => {
+    if (
+      chapter < 15 ||
+      (chapter === 15 && localStorage.getItem("lastTime") === 0 && chapter < 17)
+    ) {
+      setChapter({ type: "plus" });
+    }
+    console.log(chapter);
+  };
+
+  const handPrev = () => {
+    if (chapter > 1) {
+      setChapter({ type: "minus" });
+    } // Activated Chapter only
+    console.log(chapter);
+  };
+
+  const NextChapter = () => {
+    return (
+      <NavButtons
+        onclick={handleNext}
+        classname="nextButton"
+        disable={disableNextButton}
+      >
+        To{" "}
+        {chapter === 1
+          ? "Unit Two"
+          : chapter === 2
+          ? "Unit Three"
+          : chapter === 3
+          ? "Unit Four"
+          : chapter === 4
+          ? "Unit Five"
+          : chapter === 5
+          ? "Unit Six"
+          : chapter === 6
+          ? "Unit Seven"
+          : chapter === 7
+          ? "Unit Eight"
+          : chapter === 8
+          ? "Unit Nine"
+          : chapter === 9
+          ? "Unit Ten"
+          : chapter === 10
+          ? "Unit Eleven"
+          : chapter === 11
+          ? "Unit Twelve"
+          : chapter === 12
+          ? "Unit Thirteen"
+          : chapter === 13
+          ? "Unit Fourteen"
+          : chapter === 14
+          ? "Unit Fifteen"
+          : "Final Quiz"}
+      </NavButtons>
+    );
+  };
+
+  const PreviousChapter = () => {
+    return (
+      <NavButtons onclick={handPrev} classname="nextButton">
+        Go back to{" "}
+        {chapter === 16
+          ? "Unit Fifteen"
+          : chapter === 15
+          ? "Unit Fourteen"
+          : chapter === 14
+          ? "Unit Thirteen"
+          : chapter === 13
+          ? "Unit Twelve"
+          : chapter === 12
+          ? "Unit Eleven"
+          : chapter === 11
+          ? "Unit Ten"
+          : chapter === 10
+          ? "Unit Nine"
+          : chapter === 9
+          ? "Unit Eight"
+          : chapter === 8
+          ? "Unit Seven"
+          : chapter === 7
+          ? "Unit Six"
+          : chapter === 6
+          ? "Unit Five"
+          : chapter === 5
+          ? "Unit Four"
+          : chapter === 4
+          ? "Unit Three"
+          : chapter === 3
+          ? "Unit Two"
+          : chapter === 2
+          ? "Unit 1"
+          : "Hi"}
+      </NavButtons>
+    );
+  };
+
+  return (
+    <div>
+      <div className="TimerContainer">
+        <Timer />
+      </div>
+      {chapter === 1 ? (
+        <div className="NextButtonContainer">
+          <NextChapter />
+        </div>
+      ) : (
+        <div className="nextButtonContainer">
+          <PreviousChapter /> <NextChapter />
+        </div>
+      )}
+
+      {/* Private Route protecting chapters */}
+
+      <PrivateRoute path="/chapterone" component={ChapterOne} />
+      <PrivateRoute path="/chaptertwo" component={ChapterTwo} />
+      <PrivateRoute path="/chapterthree" component={ChapterThree} />
+      <PrivateRoute path="/chapterfour" component={ChapterFour} />
+      <PrivateRoute path="/chapterfive" component={ChapterFive} />
+      <PrivateRoute path="/chaptersix" component={ChapterSix} />
+      <PrivateRoute path="/chapterthree" component={ChapterThree} />
+      <PrivateRoute path="/chapterseven" component={ChapterSeven} />
+      <PrivateRoute path="/chaptereight" component={ChapterEight} />
+      <PrivateRoute path="/chapternine" component={ChapterNine} />
+      <PrivateRoute path="/chapterten" component={ChapterTen} />
+      <PrivateRoute path="/chaptereleven" component={ChapterEleven} />
+      <PrivateRoute path="/chapterthirteen" component={ChapterThirteen} />
+      <PrivateRoute path="/chapterfourteen" component={ChapterFourteen} />
+      <PrivateRoute path="/chapterfifteen" component={ChapterFifteen} />
+      {/* <PrivateRoute path="/finalquiz" component={FinalQuiz} /> */}
+
+      {chapter === 1 ? (
+        <redirect to="/chapterone" />
+      ) : chapter === 2 ? (
+        <redirect to="/chaptertwo" />
+      ) : chapter === 3 ? (
+        <redirect to="/chapterthree" />
+      ) : chapter === 4 ? (
+        <redirect to="/chapterfour" />
+      ) : chapter === 5 ? (
+        <redirect to="/chapterfive" />
+      ) : chapter === 6 ? (
+        <redirect to="/chaptersix" />
+      ) : chapter === 7 ? (
+        <redirect to="/chapterseven" />
+      ) : chapter === 8 ? (
+        <redirect to="/chaptereight" />
+      ) : chapter === 9 ? (
+        <redirect to="/chapternine" />
+      ) : chapter === 10 ? (
+        <redirect to="/chapterten" />
+      ) : chapter === 11 ? (
+        <redirect to="/chaptereleven" />
+      ) : chapter === 12 ? (
+        <redirect to="/chaptertwelve" />
+      ) : chapter === 13 ? (
+        <redirect to="/chapterthirteen" />
+      ) : chapter === 14 ? (
+        <redirect to="/chapterfourteen" />
+      ) : chapter === 15 ? (
+        <redirect to="/chapterfifteen" />
+      ) : null}
+    </div>
+  );
+}
--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,20 +1,88 @@
+// import { Formik, Form, Field, ErrorMessage } from "formik";
+// import { useContext, useState } from "react";
+// import AuthContext from "../contexts/Authprovider";
+// import * as Yup from "yup";
+// import "../App.css";
+// import "../styles/general.css";
+
+// export default function Signin() {
+//   const [submissionStatus, setSubmissionStatus] = useState(false);
+//   const [setAuth] = useContext(AuthContext);
+
+//   return (
+//     <div className="Signin-page">
+//       <Formik
+//         initialValues={{ email: "", password: "" }}
+//         validationSchema={Yup.object({
+//           email: Yup.string()
+//             .email("please input a valid email address")
+//             .required("Email is Required"),
+//           password: Yup.string().required("Please Input your password"),
+//         })}
+//         onSubmit={(values, { resetForm }) => {
+//           setSubmissionStatus(true);
+//           fetch("/api/signin", {
+//             method: "POST",
+//             headers: {
+//               "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify(values),
+//           }).then((response) => {
+//             console.log(response);
+//             if (response.status === 200) {
+//               const accessToken = response?.accessToken;
+//               setAuth({ accessToken });
+//               window.location.replace("/dashboard");
+//             }
+//             if (!response.ok) {
+//               alert("Invalid Email or Password");
+//               resetForm();
+//               setSubmissionStatus(false);
+//             }
+//           });
+//         }}
+//       >
+//         <Form className="signin-form">
+//           <h3 style={{ textAlign: "center", marginBottom: "10px" }}>Sign in</h3>
+//           <label htmlFor="email">Registered Email</label>
+//           <Field name="email" type="email" placeholder="Email" />
+//           <ErrorMessage name="email" />
+//           <label htmlFor="password">Password</label>
+//           <Field name="password" type="password" placeholder="Password" />
+//           <ErrorMessage name="password" />
+//           {submissionStatus ? (
+//             <button className="Signup submitting"></button>
+//           ) : (
+//             <button type="submit" className="Signup">
+//               Sign in
+//             </button>
+//           )}
+//         </Form>
+//       </Formik>
+//     </div>
+//   );
+// }
+
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import AuthContext from "../contexts/Authprovider";
 import * as Yup from "yup";
 import "../App.css";
 import "../styles/general.css";
 
 export default function Signin() {
   const [submissionStatus, setSubmissionStatus] = useState(false);
+  const [setAuth] = useContext(AuthContext);
+
   return (
     <div className="Signin-page">
       <Formik
         initialValues={{ email: "", password: "" }}
         validationSchema={Yup.object({
           email: Yup.string()
-            .email("please input a valid email address")
-            .required("Email is Required"),
-          password: Yup.string().required("Please Input your password"),
+            .email("Please input a valid email address")
+            .required("Email is required"),
+          password: Yup.string().required("Please input your password"),
         })}
         onSubmit={(values, { resetForm }) => {
           setSubmissionStatus(true);
@@ -26,25 +94,23 @@ export default function Signin() {
             body: JSON.stringify(values),
           })
             .then((response) => {
-              response.json();
               console.log(response);
               if (response.status === 200) {
-                setSubmissionStatus(true);
-                window.location.replace("/dashboard");
+                return response.json(); // Parse the response as JSON
               }
               if (!response.ok) {
-                alert("Invalid Email or Password");
-                resetForm();
-                setSubmissionStatus(false);
+                throw new Error("Invalid Email or Password");
               }
             })
             .then((data) => {
-              console.log(data.messaage);
-
-              // Check for success status or handle errors
+              const accessToken = data?.accessToken;
+              setAuth({ accessToken });
+              window.location.replace("/dashboard");
             })
             .catch((error) => {
-              console.error("Error submitting form:", error);
+              alert(error.message);
+              resetForm();
+              setSubmissionStatus(false);
             });
         }}
       >
